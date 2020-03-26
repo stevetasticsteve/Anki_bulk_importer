da9f8ef65225da40ef318995e106ebc1d6738542
steve <stevetasticsteve@gmail.com>
2020-03-26 15:09:32 +1000

    Cleared an unused import. Switched out of dev mode to merge into main branch

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ window_size = (1200, 400)
 
 # Dev mode allows a 2nd development add-on to run alongside a stable version in Anki enabling developers to develop
 # while continuing to use the stable release. Create a bulk_importer_dev folder in addons and set Dev = True to use.
-Dev = True
+Dev = False
